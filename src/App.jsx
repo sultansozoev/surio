7cb97c1aaf5a95b0ee78569b80efdb293abdcfcb
Sultan <sozoev.sultan@gmail.com>
2025-12-17 20:19:01 +0100

    fix: redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './context/AuthContext';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
